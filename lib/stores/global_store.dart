115231a994a2cbf92fe1fa4f5328098cb54335e1
João Carlos Azevedo <joaocarlosazevedo@MacBook-Pro-de-Joao.local>
2020-04-13 12:44:11 -0400

    async methods fix

--- lib/stores/global_store.dart
+++ lib/stores/global_store.dart
@@ -5,47 +5,57 @@ import 'package:mobx/mobx.dart';
 
 part 'global_store.g.dart';
 
-enum StoreState {initial,loading,loaded}
+enum StoreState { initial, loading, loaded }
 
 class GlobalApiStore = _GlobalApiStoreBase with _$GlobalApiStore;
 
 abstract class _GlobalApiStoreBase with Store {
   final CountriesRepository _globalRepository;
- 
+
   _GlobalApiStoreBase(this._globalRepository);
- 
-  @observable
-  Global globalData; 
 
   @observable
-  List<Country> rankedCountries; 
+  Global globalData;
+
+  @observable
+  List<Country> rankedCountries;
 
   @observable
   String erroMessage;
- 
-  @observable 
+
+  @observable
   ObservableFuture<List<Country>> globalDataFuture;
+
+  @observable
+  ObservableFuture<Global> globalFuture;
 
   @computed
   StoreState get state {
-    if (globalDataFuture == null || globalDataFuture.status == FutureStatus.rejected) {
-      return StoreState.initial; 
+    if (globalDataFuture == null ||
+        globalDataFuture.status == FutureStatus.rejected) {
+        return StoreState.initial;
+    }
+    if (globalDataFuture.status == FutureStatus.pending) {   
+        return StoreState.loading; 
     } 
-    return globalDataFuture.status == FutureStatus.pending ? StoreState.loading : StoreState.loaded;
-      
-    }  
-  
-  @action 
+    if (globalFuture.status == FutureStatus.fulfilled) {
+      return StoreState.loaded;
+    } 
+  }
+
+  @action
   Future getGlobalData() async {
-    try{
+    try {
       erroMessage = null;
-      globalDataFuture =  ObservableFuture(_globalRepository.fetchAllCountries());  
-      globalData      = await _globalRepository.fetchGlobalData();
-      var countries   = await globalDataFuture; 
-      countries.sort((a,b) => b.cases.compareTo(a.cases));
+      globalDataFuture =
+          ObservableFuture(_globalRepository.fetchAllCountries());
+      globalFuture = ObservableFuture(_globalRepository.fetchGlobalData());
+      globalData = await globalFuture;
+      var countries = await globalDataFuture;
+      countries.sort((a, b) => b.cases.compareTo(a.cases));
       rankedCountries = countries.take(10).toList();
-    }catch(error) {
-      erroMessage = error; 
+    } catch (error) {
+      erroMessage = error;
     }
   }
 }
