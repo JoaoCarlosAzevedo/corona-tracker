d4f29826d790b51790a858d0e745eda1998ad4ee
João Carlos Azevedo <joaocarlosazevedo@MacBook-Pro-de-Joao.local>
2020-04-28 12:32:50 -0400

    Bottom bar fix

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:corona_tracker/pages/home_page/widgets/bottom_appbar.dart';
-import 'package:corona_tracker/pages/home_page/widgets/float_button.dart';
 import 'package:corona_tracker/pages/home_page/widgets/loadDataWidgets.dart';
 import 'package:corona_tracker/pages/home_page/widgets/loading_page.dart';
 import 'package:corona_tracker/stores/global_store.dart';
@@ -14,6 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   GlobalApiStore _globalDataStore;
+  int _currentPage;
 
   @override
   void didChangeDependencies() {
@@ -22,52 +22,83 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    _currentPage = 0;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: <Widget>[
-                Observer(
-                  builder: (_) {
-                    switch (_globalDataStore.state) {
-                      case StoreState.initial: 
-                        return Center(
-                          child: Text("Initial State",
-                              style: TextStyle(color: Colors.white)),
-                        );
-                      case StoreState.loading:
-                        return LoadingPage(); 
-                      case StoreState.loaded:
-                        return LoadDataWidgets(_globalDataStore);
-                      default:
-                        {
-                          return Text("Algum erro $_globalDataStore.erroMessage",
+      body: getPage(_currentPage),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        final globaDataStore =
+            Provider.of<GlobalApiStore>(context, listen: false);
+        globaDataStore.getGlobalData();
+      }),
+      bottomNavigationBar: AnimatedBottomNav(
+          currentIndex: _currentPage,
+          onChange: (index) {
+            setState(() {
+              _currentPage = index;
+            });
+          }),
+    );
+  }
+
+  getPage(int page) {
+    switch (page) {
+      case 0:
+        return Center(
+            child: Container(
+          child: _mainPage(),//Text("Home Page"),
+        ));
+      case 1:
+        return Center(
+            child: Container(
+          child: Text("Profile Page"),
+        ));
+      case 2:
+        return Center(
+            child: Container(
+          child: Text("Menu Page"),
+        ));
+    }
+  }
+
+  Widget _mainPage() {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Observer(
+                builder: (_) {
+                  switch (_globalDataStore.state) {
+                    case StoreState.initial:
+                      return Center(
+                        child: Text("Initial State",
+                            style: TextStyle(color: Colors.white)),
+                      );
+                    case StoreState.loading:
+                      return LoadingPage();
+                    case StoreState.loaded:
+                      return LoadDataWidgets(_globalDataStore);
+                    default:
+                      {
+                        return Text("Algum erro $_globalDataStore.erroMessage",
                             style: TextStyle(color: Colors.white));
-                        }
-                    }
-                  },
-                ),
-              ],
-            ),
+                      }
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        final globaDataStore = Provider.of<GlobalApiStore>(context, listen: false);
-        globaDataStore.getGlobalData();
-      }),
-      bottomNavigationBar: CustomBottomBar(),
-     /*  bottomNavigationBar: CustomBottomAppBar(),
-      floatingActionButton: SizedBox(
-        width: 100,
-        height: 100,
-        child: CustomFloatButtom(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, */
     );
   }
 }
+
--- lib/pages/home_page/widgets/bottom_appbar.dart
+++ lib/pages/home_page/widgets/bottom_appbar.dart
@@ -1,30 +1,114 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-class CustomBottomBar extends StatefulWidget {
-  @override
-  _CustomBottomBarState createState() => _CustomBottomBarState();
-}
 
-class _CustomBottomBarState extends State<CustomBottomBar> {
 
+class AnimatedBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onChange;
+  const AnimatedBottomNav({Key key, this.currentIndex, this.onChange})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return CurvedNavigationBar(
-    index: 1,
-    backgroundColor: Theme.of(context).primaryColor,
-    height: 50,
-    color: Theme.of(context).accentColor,
-    animationDuration: Duration(milliseconds: 300),
-    animationCurve: Curves.easeOutCubic,
-    items: <Widget>[
-      Icon(Icons.explore, size: 30, color: Colors.white,),
-      Icon(Icons.public, size: 30, color: Colors.white),
-      Icon(Icons.search, size: 30, color: Colors.white),
-    ],
-    onTap: (index) {
-      //Handle button tap
-      print(index);
-    },
-  );
+    return Container(
+      height: kToolbarHeight,
+      decoration: BoxDecoration(color: Theme.of(context).accentColor),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: InkWell(
+              onTap: () => onChange(0),
+              child: BottomNavItem(
+                icon: Icons.public,
+                title: "Global",
+                isActive: currentIndex == 0,
+                activeColor: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () => onChange(1),
+              child: BottomNavItem(
+                icon: Icons.explore,
+                title: "Regional",
+                isActive: currentIndex == 1,
+                activeColor: Colors.white,
+              ),
+            ),
+          ),
+          /* Expanded(
+            child: InkWell(
+              onTap: () => onChange(2),
+              child: BottomNavItem(
+                icon: Icons.menu,
+                title: "Menu",
+                isActive: currentIndex == 2,
+              ),
+            ),
+          ), */
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final bool isActive;
+  final IconData icon;
+  final Color activeColor;
+  final Color inactiveColor;
+  final String title;
+  const BottomNavItem(
+      {Key key,
+      this.isActive = false,
+      this.icon,
+      this.activeColor,
+      this.inactiveColor,
+      this.title})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      transitionBuilder: (child, animation) {
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0.0, 1.0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: child,
+        );
+      },
+      duration: Duration(milliseconds: 500),
+      reverseDuration: Duration(milliseconds: 200),
+      child: isActive
+          ? Container(
+              color: Theme.of(context).accentColor,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: activeColor ?? Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  const SizedBox(height: 5.0),
+                  Container(
+                    width: 5.0,
+                    height: 5.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: activeColor ?? Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Icon(
+              icon,
+              color: inactiveColor ?? Colors.grey,
+            ),
+    );
   }
 }
